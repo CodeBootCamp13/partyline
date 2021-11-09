c3f8e91a72e720130da63047e65fc70116ba67bd
Karif Williams <kariftw@hotmail.com>
2021-11-09 16:51:06 -0500

    res.render() updates

--- app.js
+++ app.js
@@ -17,31 +17,45 @@ app.use( express.static('public') );
 app.use( express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-	//displays users parties by username (/username)
-	app.get('/', (req, res) => {
-	
-	});
+//displays main screen
+app.get('/', (req, res) => {
+	res.render('home')
+    
+});
 
-	//displays selected party messages (/username/party)
-	app.get('/', (req, res) => {
-	
-	});
-
-	//create a new party
-	app.post('/newParty', (req, res) => {
-	
-	});
-
-  	//create a new message
-	app.post('/party/newMessage', (req, res) => {
+//displays users parties by username (/username)
+app.get('/:username', (req, res) => {
+	res.render('users')
+    
 	
 });
 
-	//deleting parties
-	app.delete('/newParty', (req, res) => {
-	
-	});
+//displays selected party messages (/username/party)
+app.get('/:username/party', (req, res) => {
+	res.render('party')
+    
+});
 
-	// create a route for part search function
+//create a new party
+app.post('/:newParty', (req, res) => {
+	res.render('newParty')
+    
+});
+
+//create a new message
+app.post('/:party/partyId', (req, res) => {
+	res.render('newMessage')
+    
+});
+
+//deleting parties
+app.delete('/:party/partyId', (req, res) => {
+	res.render('deleteParty')
+    
+});
+
+// create a route for part search function
+
+
 
 app.listen(3000, () => console.log('Server is up on port 3000'))
