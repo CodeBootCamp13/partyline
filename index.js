4ed20b938ea0d006478a950e8dc6399438f21dad
Josh <jwheeler5@protonmail.com>
2021-11-18 18:00:51 -0500

    75_Josh

--- index.js
+++ index.js
@@ -293,4 +293,12 @@ app.get('/user/:user_id', (req, res) => {
 	});
 });
 
+app.get('/api/party/:party_id/:message_id'),(req,res) => {
+	connection.query('SELECT id, party_id, message FROM messages WHERE party_id = ?'), [ req.params.party_id ], (err, results) => {
+		let currentMessages = results.message_id
+
+		if(req.params.message_id < currentMessage)
+	}
+};
+
 app.listen(3000, () => console.log('Server is up on port 3000'))
