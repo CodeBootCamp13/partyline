712bc238d1fbe06697288bd8790a4e84f1f10f28
Eric Harrison <github@hrrsn.co>
2021-12-01 18:16:54 -0500

    Adds process.env.PORT for Heroku

--- index.js
+++ index.js
@@ -333,4 +333,4 @@ app.get('/api/party/:party_id/:message_id' ,(req,res) => {
 	});
 });
 
-app.listen(3000, () => console.log('Server is up on port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Server is up on port 3000'));
